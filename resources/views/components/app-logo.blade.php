31d671ce37d88faa60c723218b7734bf569ba181
abela-a <work.abelardhana@gmail.com>
2025-06-21 18:48:51 +0800

    Refactor app logo component for improved structure and dynamic application name

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent text-accent-content">
-    <x-app-logo-icon class="size-5 fill-current text-base-200" />
+<div class="bg-accent text-accent-content flex aspect-square size-8 items-center justify-center rounded-md">
+    <x-app-logo-icon class="text-base-200 size-5 fill-current" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Mary UI Starter Kit</span>
+    <span class="mb-0.5 truncate font-semibold leading-none">{{ config('app.name') }}</span>
 </div>
