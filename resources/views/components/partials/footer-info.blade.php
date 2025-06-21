23f3747afe88be636f7106e8a22775ec9c584974
abela-a <work.abelardhana@gmail.com>
2025-06-21 18:18:42 +0800

    Update footer component to reflect new attribution and improve formatting

--- resources/views/components/partials/footer-info.blade.php
+++ resources/views/components/partials/footer-info.blade.php
@@ -1,9 +1,19 @@
 <footer {{ $attributes->merge(['class' => 'mt-auto text-center']) }}>
     <x-mary-menu-separator />
-    <p class="text-xs text-base-content/70">
-        &copy; {{ date('Y') }} <span class="font-bold">{{ config('app.name', 'Laravel') }}</span>. Made
-        with <x-mary-icon name="o-heart" class="inline h-4 w-4 text-error" /> by
-        <a href="https://lauroguedes.dev" target="_blank" rel="noopener noreferrer"
-            class="text-primary hover:underline">Lauro Guedes</a>.
+
+    <p class="text-base-content/70 text-xs">
+        &copy; {{ date('Y') }} <span class="font-bold">{{ config('app.name', 'Laravel') }}</span>.
+        Made with
+        <x-mary-icon
+            class="text-error inline h-4 w-4"
+            name="o-heart"
+        />
+        by
+        <a
+            class="text-primary hover:underline"
+            href="https://github.com/fukigenmedia"
+            target="_blank"
+            rel="noopener noreferrer"
+        >Fukigen Media</a>.
     </p>
 </footer>
