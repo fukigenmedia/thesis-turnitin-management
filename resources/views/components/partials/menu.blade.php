bf0acd7e35f9e5dfb0f4413a13f33ed73ed2acb5
abela-a <work.abelardhana@gmail.com>
2025-06-22 16:37:23 +0800

    Placeholder for future menu

--- resources/views/components/partials/menu.blade.php
+++ resources/views/components/partials/menu.blade.php
@@ -8,9 +8,39 @@
         :link="route('dashboard')"
     />
     <x-mary-menu-item
+        title="Berkas Turnitin"
+        icon="s-bookmark-square"
+        :link="route('users.index')"
+    />
+    <x-mary-menu-item
         title="Pengguna"
         icon="s-users"
         :link="route('users.index')"
         :hidden="!in_array(auth()->user()->role->value, ['admin'])"
     />
+    <x-mary-menu-item
+        title="Slider"
+        icon="s-photo"
+        :link="route('users.index')"
+        :hidden="!in_array(auth()->user()->role->value, ['admin'])"
+    />
+    <li class="self-center">
+        <x-mary-dropdown
+            class="!p-0"
+            no-x-anchor
+            right
+        >
+            <x-slot:trigger>
+                <x-mary-icon name="s-bell" />
+                <x-mary-badge
+                    class="badge-neutral badge-sm"
+                    value="+99"
+                />
+            </x-slot:trigger>
+
+            <div>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, laudantium?
+            </div>
+        </x-mary-dropdown>
+    </li>
 </x-mary-menu>
